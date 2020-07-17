3c9322ce4e972910cf282555899bbb5c92f2f1a1
von Buelow <jaspar@UMICH.EDU>
2020-07-17 19:01:50 -0400

    Chips component

---
+++ src/Chips.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import chipsImg from "./chips.jpg";
+
+
+function Chips() {
+    return (
+        <div className="Chips">
+            <p>Here are your chips</p>
+            <img style={{width: 300}} src={chipsImg}/>
+            <p><Link exact to="/">Go Back</Link></p>
+        </div>
+    );
+}
+
+export default Chips;
--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 function VendingMachine() {
     return (
         <div className="VendingMachine">
             <div><p>Hello I'm a Vending Machine. What would you like to eat?</p></div>
-            <nav className="MyNavBarClass">
-                <NavLink exact to="/snickers">Snickers</NavLink>
-                <NavLink exact to="/chips">Chips</NavLink>
-                <NavLink exact to="/drpepper">Dr Pepper</NavLink>
+            <nav className="VendingMachine-nav">
+                <p><Link exact to="/snickers">Snickers</Link></p>
+                <p><Link exact to="/chips">Chips</Link></p>
+                <p><Link exact to="/drpepper">Dr Pepper</Link></p>
             </nav>
         </div>
     );
