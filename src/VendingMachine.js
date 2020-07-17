68adadc50214ca9bd6589368866e3dee8f0eee6b
von Buelow <jaspar@UMICH.EDU>
2020-07-17 18:26:47 -0400

    VendingMachine component

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+
 
 function VendingMachine() {
-  return (
-    <div className="VendingMachine">
-      
-    </div>
-  );
+    return (
+        <div className="VendingMachine">
+            <div><p>Hello I'm a Vending Machine. What would you like to eat?</p></div>
+            <nav className="MyNavBarClass">
+                <NavLink exact to="/snickers">Snickers</NavLink>
+                <NavLink exact to="/chips">Chips</NavLink>
+                <NavLink exact to="/drpepper">Dr Pepper</NavLink>
+            </nav>
+        </div>
+    );
 }
 
 export default VendingMachine;
