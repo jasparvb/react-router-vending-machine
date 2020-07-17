732991a8227bdbc5de03b88d1bb97fd73328c204
von Buelow <jaspar@UMICH.EDU>
2020-07-17 18:13:55 -0400

    App routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
+import VendingMachine from './VendingMachine';
+import Snickers from './Snickers';
+import Chips from './Chips';
+import DrPepper from './DrPepper';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Route exact path="/">
+          <VendingMachine />
+        </Route>
+        <Route exact path="/snickers">
+          <Snickers />
+        </Route>
+        <Route exact path="/chips">
+          <Chips />
+        </Route>
+        <Route exact path="/drpepper">
+          <DrPepper />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/VendingMachine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function VendingMachine() {
+  return (
+    <div className="VendingMachine">
+      
+    </div>
+  );
+}
+
+export default VendingMachine;
